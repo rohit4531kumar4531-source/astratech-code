b5f42bd9bfdd5ffb17d1d2d5e3925d6e1c2e7092
rohit kumar <rohit4531kumar4531@gmail.com>
2026-05-24 10:35:05 +0530

    sum

---
+++ question5.js
@@ -0,0 +1,6 @@
+const num = 5;
+let sum = 0;
+for (let i = num; i >= 1; i--) {
+  sum += i;
+}
+console.log(sum);
--- questionFIZZ3.js
+++ questionFIZZ3.js
@@ -1,9 +1,9 @@
 const num = 1;
 for (i = 1; i <= 100; i++)
-  if (i % 3 == 0) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("fizzbuzz");
+  } else if (i % 3 == 0) {
     console.log("fizz");
   } else if (i % 5 == 0) {
     console.log("buzz");
-  } else if (i % 3 == 0 && i % 5 == 0) {
-    console.log("fizzbuzz");
   } else console.log(i);
