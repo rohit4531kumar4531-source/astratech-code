1aa7ad0b739171c2f4ae23d28154ee9bc26a4006
rohit kumar <rohit4531kumar4531@gmail.com>
2026-05-24 09:47:28 +0530

    table of 32

--- loop.js
+++ loop.js
@@ -1,11 +1,4 @@
-const table = 593;
-console.log(table * 1);
-console.log(table * 2);
-console.log(table * 3);
-console.log(table * 4);
-console.log(table * 5);
-console.log(table * 6);
-console.log(table * 7);
-console.log(table * 8);
-console.log(table * 9);
-console.log(table * 10);
+const table = 32;
+for (i = 1; i <= 10; i++) {
+  console.log(table * i);
+}
