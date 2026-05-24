a8a951329f96c2b13c704000b738f6de02c5634e
rohit kumar <rohit4531kumar4531@gmail.com>
2026-05-24 09:17:48 +0530

    elseif problem

---
+++ ifelse.js
@@ -0,0 +1,9 @@
+const age=32
+if(age<19)
+    console.log("not approverd")
+else if(age>19 && age<=60)
+    console.log("approved")
+else if(age>=61&& age<=150)
+    console.log("senior not approved")
+else
+    console.log("you are dead")
