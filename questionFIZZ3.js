668cbe2323a6534de12410a13817b93c70b500f2
rohit kumar <rohit4531kumar4531@gmail.com>
2026-05-24 10:10:36 +0530

    fizzbuzz

---
+++ questionFIZZ3.js
@@ -0,0 +1,9 @@
+const num = 1;
+for (i = 1; i <= 100; i++)
+  if (i % 3 == 0) {
+    console.log("fizz");
+  } else if (i % 5 == 0) {
+    console.log("buzz");
+  } else if (i % 3 == 0 && i % 5 == 0) {
+    console.log("fizzbuzz");
+  } else console.log(i);
